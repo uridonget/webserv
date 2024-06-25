4f5cf0a8b7289a1cce86b743728bea4952133500
heolee <heolee@student.42seoul.kr>
2024-06-25 17:23:38 +0900

    [MOD] iomulti

--- src/IoMultiplexing.cpp
+++ src/IoMultiplexing.cpp
@@ -23,38 +23,3 @@
 #include <vector>
 #include "../include/Webserv.hpp"
 
-static void set_nonblocking(int sockfd) {
-	fcntl(sockfd, F_SETFL, O_NONBLOCK);
-}
-
-static int init_server(int server_fd) {
-	int opt = 1;
-	struct sockaddr_in address;
-
-	set_nonblocking(server_fd);
-	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) == -1) {
-		throw RuntimeException("setsockopt");
-	}
-
-	std::memset(&address, 0, sizeof(address));
-	address.sin_family = AF_INET;
-	address.sin_addr.s_addr = INADDR_ANY;
-	address.sin_port = htons(8080);                                              // 여기 변수로 꽂아야함
-
-	if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) == -1) {
-		throw RuntimeException("bind");
-	}
-
-	if (listen(server_fd, 10) == -1) {
-		throw RuntimeException("listen");
-	}
-
-	int kq = kqueue();
-	if (kq == -1) {
-		throw RuntimeException("kqueue");
-	}
-
-	return kq;
-}
-
-

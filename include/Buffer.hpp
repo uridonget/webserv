2d748dd533da887738911653986595ca66b5abca
heolee <heolee@student.42seoul.kr>
2024-06-25 17:20:19 +0900

    initial

---
+++ include/Buffer.hpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Buffer.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: heolee <heolee@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/25 16:27:05 by heolee            #+#    #+#             */
+/*   Updated: 2024/06/25 16:27:07 by heolee           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUFFER_HPP
+# define BUFFER_HPP
+
+#include <vector>
+
+class Buffer {
+
+	protected:
+		int fd;
+		std::vector<char> buffer;
+
+	public:
+		Buffer();
+		virtual ~Buffer();
+		// Buffer(const Buffer& other);
+		// Buffer& operator=(const Buffer& other);
+
+
+};
+
+#endif
+
+
+
+
